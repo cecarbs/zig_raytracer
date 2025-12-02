7f91fceb0ab0a270b2063598646471538e209d05
cecarbs <carbonel257@gmail.com>
2025-12-01 20:49:43 -0600

    Create an image file

--- src/main.zig
+++ src/main.zig
@@ -3,25 +3,30 @@ const raytracer = @import("raytracer");
 
 pub fn main() !void {
     // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try raytracer.bufferedPrint();
-}
+    // std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    // try raytracer.bufferedPrint();
+    const image_width = 256;
+    const image_height = 256;
 
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
+    var stdout_buffer: [1024]u8 = undefined;
+    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
+    const stdout = &stdout_writer.interface;
 
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
+    try stdout.print("P3\n{d} {d}\n255\n", .{ image_width, image_height });
+
+    for (0..image_height) |j| {
+        for (0..image_width) |i| {
+            const r = @as(f64, @floatFromInt(i)) / @as(f64, image_width - 1);
+            const g = @as(f64, @floatFromInt(j)) / @as(f64, image_height - 1);
+            const b: f64 = 0.0;
+
+            const ir = @as(u8, @intFromFloat(255.999 * r));
+            const ig = @as(u8, @intFromFloat(255.999 * g));
+            const ib = @as(u8, @intFromFloat(255.999 * b));
+
+            try stdout.print("{d} {d} {d}\n", .{ ir, ig, ib });
         }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+    }
+
+    try stdout.flush();
 }
