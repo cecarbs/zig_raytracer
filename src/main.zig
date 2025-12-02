969fe4f21cc0db2f8849bd82143be447923f895d
cecarbs <carbonel257@gmail.com>
2025-12-02 07:19:11 -0600

    Add progress indicator

--- src/main.zig
+++ src/main.zig
@@ -2,9 +2,6 @@ const std = @import("std");
 const raytracer = @import("raytracer");
 
 pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    // std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    // try raytracer.bufferedPrint();
     const image_width = 256;
     const image_height = 256;
 
@@ -12,10 +9,18 @@ pub fn main() !void {
     var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
     const stdout = &stdout_writer.interface;
 
+    var stderr_buffer: [1024]u8 = undefined;
+    var stderr_writer = std.fs.File.stderr().writer(&stderr_buffer);
+    const stderr = &stderr_writer.interface;
+
     try stdout.print("P3\n{d} {d}\n255\n", .{ image_width, image_height });
 
     for (0..image_height) |j| {
         for (0..image_width) |i| {
+            // std.debug.print("\rScanlines remaining: {d} ", .{image_height - j});
+            try stderr.print("\rScanlines remaining: {d} ", .{image_height - j});
+            try stdout.flush();
+
             const r = @as(f64, @floatFromInt(i)) / @as(f64, image_width - 1);
             const g = @as(f64, @floatFromInt(j)) / @as(f64, image_height - 1);
             const b: f64 = 0.0;
@@ -29,4 +34,7 @@ pub fn main() !void {
     }
 
     try stdout.flush();
+    // std.debug.print("\rDone.                 \n");
+    try stderr.print("\rDone.                 \n", .{});
+    try stderr.flush();
 }
